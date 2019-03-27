fc0bebb6877dc43d079e557295c45ff1d74b1c6a
dclements9 <dclements@protonmail.com>
2019-03-27 08:35:29 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,16 @@
 class Pokemon
+    attr_accessor = :id, :name, :type, :db
+    #database_connection = SQLite3::Database.new('db/pokemon.db')
+
+    def initialize(id, name, type, db)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+        @@all << self
+    end
+
+    def save(name, type, db)
+
+    end
 end
