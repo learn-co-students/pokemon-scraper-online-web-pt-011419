2e6f10b62a8331d95a2a1705369e26081319754d
dylanclements <dclements@protonmail.com>
2019-03-27 12:48:45 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,16 +1,16 @@
+require 'pry'
+
 class Pokemon
     attr_accessor = :id, :name, :type, :db
-    #database_connection = SQLite3::Database.new('db/pokemon.db')
 
-    def initialize(id, name, type, db)
+    def initialize (id:, name:, type:, db:)
         @id = id
         @name = name
         @type = type
         @db = db
-        @@all << self
     end
-
-    def save(name, type, db)
-
-    end
+    #binding.pry
+    # def save(name, type, db)
+    #     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+    # end
 end
